b66c2bc77b650007bd6b4037871e262d5acd99c2
salma bouaouid <salmabouaouid57@gmail.com>
2020-05-06 15:28:24 +0000

    Controllers added

---
+++ app/Http/Controllers/HomeworksController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Homework;
+use Illuminate\Http\Request;
+
+class HomeworksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Homework  $homework
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Homework $homework)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Homework  $homework
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Homework $homework)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Homework  $homework
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Homework $homework)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Homework  $homework
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Homework $homework)
+    {
+        //
+    }
+}
