0f2f552b81d1f7453de92fe14aca4a3612fda4bd
salma bouaouid <salmabouaouid57@gmail.com>
2020-05-06 15:30:15 +0000

    Factories added

---
+++ database/factories/ContributionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Contribution;
+use Faker\Generator as Faker;
+
+$factory->define(Contribution::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
