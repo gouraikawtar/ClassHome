a9460752a5389dedf7fff8298bba398bba8d2315
gouraikawtar <k.gourai14@gmail.com>
2020-07-06 23:42:14 +0100

    updating css and js files with laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,8 @@ const mix = require('laravel-mix');
 |
  */
 
-// mix.js('resources/js/app.js', 'public/js')
-//     .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
 
 mix.styles('resources/css/teacher.css', 'public/css/myCustomTheme.css').version();
 mix.styles('resources/css/membership.css', 'public/css/formsTheme.css').version();
